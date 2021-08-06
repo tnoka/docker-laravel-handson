7b41bec6179b08007edc2fc86f383b57b1634e7a
Tsutomu-Nagaoka <mxx.2963@icloud.com>
2021-08-07 00:06:34 +0900

    fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// TwitterAPI ソーシャルログイン機能
+// TwitterAPI ソーシャルログイン機能ass
 Route::get('auth/twitter', 'Auth\SocialAuthController@redirectToProvider');
 Route::get('auth/twitter/callback', 'Auth\SocialAuthController@handleProviderCallback');
 Route::get('auth/twitter/logout', 'Auth\SocialAuthController@logout');
